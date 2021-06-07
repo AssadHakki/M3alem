843c3a2317fcf9e1ce86a9f3931b8b3b34612a31
AssadHakki <hakki.assad@gmail.com>
2021-06-07 13:04:56 +0100

    fixed Error 500 when the listener refused the connection

--- src/java/M3alemBackend/connexion.java
+++ src/java/M3alemBackend/connexion.java
@@ -1,11 +1,36 @@
-
 package M3alemBackend;
 
 import java.sql.*;
-public class connexion
-{
-    
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+public class connexion {
+    
+    /*
+    Best way to avoid error: 
+        java.sql.SQLException: Listener refused the connection with the following error: ORA-12519,
+        TNS:no appropriate service handler found The Connection descriptor used by the client was: localhost:1521:xe
+    is not to connect all the time to database, just once will do.
+     */
+    private static Connection conn;
+
+    public static Connection Seconnecter() {
+        try {
+            if (conn == null) {
+                Class.forName("oracle.jdbc.driver.OracleDriver");
+
+                conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe",  "m3alem", "123");
+
+                System.out.println("Successfully connected to oracle database.");
+            }
+            return conn;
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(connexion.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+
+    /*
    // Connect to Oracle Database
     public static Connection Seconnecter()
     {
@@ -33,6 +58,5 @@ public class connexion
         // Failed to connect
         return null;
     }
-
-
+     */
 }
